34f891ac96ca57e1811c53bbd2f145da9b6e8f2f
mohammad-sazandeh <mohamad.sazandeh84@gmail.com>
2025-08-26 13:39:01 +0330

    Add data

---
+++ src/data.js
@@ -0,0 +1,5 @@
+export const users = [
+  { id: 1, name: "علی", email: "ali@example.com", role: "Admin" },
+  { id: 2, name: "زهرا", email: "zahra@example.com", role: "User" },
+  { id: 3, name: "محمد", email: "mohammad@example.com", role: "Editor" },
+];
