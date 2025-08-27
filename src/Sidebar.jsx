f8fd7a77d7c233e665e8c6bdd9e298ce59457705
mohammad-sazandeh <mohamad.sazandeh84@gmail.com>
2025-08-27 12:01:25 +0330

    Add sidebar.jsx

---
+++ src/Sidebar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <h2>منو</h2>
+      <ul>
+        <li>🏠 داشبورد</li>
+        <li>👥 کاربران</li>
+        <li>⚙️ تنظیمات</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
